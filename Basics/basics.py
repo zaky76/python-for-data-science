ed73dcf4f82226f7462d7267c48334543a1c7fa0
Zaky Hossain <zaky.cse@gmail.com>
2026-04-25 15:42:30 +0600

    first commit

---
+++ Basics/basics.py
@@ -0,0 +1,13 @@
+
+# Data Structure --List--
+tech_stack = ["Laravel", "React", "Python", "MySQL"]
+
+tech_stack.append("Pandas") 
+
+tech_stack.insert(1, "PostgreSQL") 
+
+# (Slicing) 
+top_two = tech_stack[0:2] 
+
+print(f"Complete Stack: {tech_stack}")
+print(f"Top 2 Tech: {top_two}")
