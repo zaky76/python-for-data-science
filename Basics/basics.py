0cddaf0d074656dd2d85531215455f9adad81e7a
Zaky Hossain <zaky.cse@gmail.com>
2026-05-02 17:08:01 +0600

    wokring stuctural topic

--- Basics/basics.py
+++ Basics/basics.py
@@ -1,13 +1,37 @@
 
 # Data Structure --List--
-tech_stack = ["Laravel", "React", "Python", "MySQL"]
+# tech_stack = ["Laravel", "React", "Python", "MySQL"]
 
-tech_stack.append("Pandas") 
+# tech_stack.append("Pandas") 
 
-tech_stack.insert(1, "PostgreSQL") 
+# tech_stack.insert(1, "PostgreSQL") 
 
-# (Slicing) 
-top_two = tech_stack[0:2] 
+# # (Slicing) 
+# top_two = tech_stack[0:2] 
 
-print(f"Complete Stack: {tech_stack}")
-print(f"Top 2 Tech: {top_two}")
+# print(f"Complete Stack: {tech_stack}")
+# print(f"Top 2 Tech: {top_two}")
+
+
+
+
+study_hours = 1.5
+is_consistent = True
+
+if study_hours >= 1.5 and is_consistent:
+    print("Goal: You will become a Data Scientist soon!")
+elif study_hours > 0:
+    print("Keep going, increase your time slowly.")
+else:
+    print("Start coding today!")
+
+
+    study_hours = 1.5
+is_consistent = True
+
+if study_hours >= 1.5 and is_consistent:
+    print("Goal: You will become a Data Scientist soon!")
+elif study_hours > 0:
+    print("Keep going, increase your time slowly.")
+else:
+    print("Start coding today!")
