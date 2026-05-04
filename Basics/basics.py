46454cdd9d4439b4ba40666efe0b92a66300f716
Zaky Hossain <zaky.cse@gmail.com>
2026-05-04 17:08:12 +0600

    some changes

--- Basics/basics.py
+++ Basics/basics.py
@@ -44,3 +44,21 @@ elif study_hours > 0:
     print("Keep going, increase your time slowly.")
 else:
     print("Start coding today!")
+
+
+    
+if study_hours >= 1.5 and is_consistent:
+    print("Goal: You will become a Data Scientist soon!")
+elif study_hours > 0:
+    print("Keep going, increase your time slowly.")
+else:
+    print("Start coding today!")
+    study_hours = 1.5
+is_consistent = True
+
+if study_hours >= 1.5 and is_consistent:
+    print("Goal: You will become a Data Scientist soon!")
+elif study_hours > 0:
+    print("Keep going, increase your time slowly.")
+else:
+    print("Start coding today!")
